9fd82128a97418939495d418d2fa0476956ae0b0
jisoo <jisoo.lee.susu@gmail.com>
2019-04-29 21:26:59 +0900

    initial commit 0429

---
+++ festival0429/src/main/java/festival0429/IndexController.java
@@ -0,0 +1,14 @@
+package festival0429;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "mainPage";
+	}
+	
+}
